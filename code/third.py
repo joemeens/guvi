c43d658a0774a49d06d7d3389a0da9d70ce8e002
joemeens <51346104+joemeens@users.noreply.github.com>
2019-06-04 12:59:00 +0530

    Create third.py

---
+++ code/third.py
@@ -0,0 +1,10 @@
+j=input()
+if(j>="a" and j<="z") or (j>="A" and j<="Z"):
+    if(j=="a" or j=="e"or j=="i"or j=="o"or j=="u"):
+        print("Vowel")
+    elif(j=="A" or j=="E" or j=="I" or j=="O" or j=="U"):
+        print("Vowel")
+    else:
+        print("Consonant")
+else:
+    print("invalid")
