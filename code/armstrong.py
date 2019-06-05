3e080ad3d072ca41d74123fad9250ffc697fb0aa
joemeens <51346104+joemeens@users.noreply.github.com>
2019-06-05 21:17:45 +0530

    Create armstrong.py

---
+++ code/armstrong.py
@@ -0,0 +1,15 @@
+los=input()
+los=int(los)
+digit=0
+temp=los
+
+while(temp<0):
+    rem=temp%10
+    digit=digit+(rem**3)
+    temp=temp//10
+
+
+if(los==digit):
+    print("yes")
+else:
+    print("NO")
